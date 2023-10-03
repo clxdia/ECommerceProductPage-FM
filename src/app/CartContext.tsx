0693ba12af6c43fdafa37d7af78874749b636310
blue cat <01claudia.parravano@gmail.com>
2023-10-03 15:38:46 +0200

    add badge

--- src/app/CartContext.tsx
+++ src/app/CartContext.tsx
@@ -9,32 +9,21 @@ export const CartProvider = ({ children }) => {
     const products = useSelector((state) => state.cart.products);
     const [cartOpen, setCartOpen] = useState(false);
 
-    const [notif, setNotif] = useState();
-
-    const handleQuantity = () => {
-        products.forEach((item) => {
-            setNotif((prevNotif) => prevNotif + item.quantity);
-        });
-    };
-
-    console.log("notifcc" + notif);
-
     const toggleCart = () => {
         setCartOpen(!cartOpen);
 
         setTimeout(() => {
             setCartOpen(cartOpen);
         }, 3000);
-        console.log(notif);
     };
 
-    const removeCartNotif = () => {
-        if (notif === 1) {
-            setNotif(false);
-        } else {
-            setNotif(notif - 1);
-        }
-    };
+    // const removeCartNotif = () => {
+    //     if (notif === 1) {
+    //         setNotif(false);
+    //     } else {
+    //         setNotif(notif - 1);
+    //     }
+    // };
 
     const handleMouseEnter = () => {
         setCartOpen(true);
@@ -49,8 +38,8 @@ export const CartProvider = ({ children }) => {
             value={{
                 cartOpen,
                 toggleCart,
-                notif,
-                removeCartNotif,
+                // notif,
+                // removeCartNotif,
                 handleMouseEnter,
                 handleMouseLeave,
             }}
